cb2bbdfc5e81e165bdbbbf95afd0ddaa0035440d
Vinit Morankar <121604759+Mvinit2245@users.noreply.github.com>
2025-08-05 22:57:08 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 
 public class Hello {
      public static void main(String args[]) {
-    	 System.out.println("Hello vinit morankar");
+    	 System.out.println("Hello vinit Morankar, Today is Tuesday!");
      }
 }
